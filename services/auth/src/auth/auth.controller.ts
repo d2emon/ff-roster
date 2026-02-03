9c64222456b003108f8fc29e9f419265b09c0fcd
Dmitry Kutsenko <d2emonium@gmail.com>
2026-02-03 11:49:44 +0300

    Add auth health checker

--- services/auth/src/auth/auth.controller.ts
+++ services/auth/src/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  @Get('health')
+  health() {
+    return {
+      status: 'ok',
+      service: 'auth',
+    };
+  }
+}
